42645b7f635de3e2410bf9a4b3268176d10befe8
Stenio Rodrigues <30318192+oinetsrv@users.noreply.github.com>
2024-06-05 18:10:31 -0300

    atualização da partida bem sucedida
    está quase no final do protocolo de partida. em vez de fazer full duplex eu apliquei um delay equivalente ao tempo de resposta e apontei a resposta esperada. em momentos criticos eu solicitei a leitura da porta e comparei o vetor.

--- SERIAL-RS232_DEBUG/SERIAL-RS232_DEBUG.ino
+++ SERIAL-RS232_DEBUG/SERIAL-RS232_DEBUG.ino
@@ -22,81 +22,74 @@
 // --- Variáveis Globais            ---
 // mensagens boot
 
-byte msg_boot1 [] = {0x00};
-byte msg_boot2 [] = {0x41,0x21};
-byte msg_boot3 [] = {0x40};
-byte msg_boot4 [] = {0x00,0x42};
-byte msg_boot5 [] = {0x00,0x50,0x43,0x6F,0x6E,0x65,0x78,0x61,0x6F,0x20,0x63,0x6F,0x72,0x72,0x65,0x74,0x61,0x20};
-byte msg_boot6 [] = {0x00,0x04};
-byte msg_boot7 [] = {0x00,0x4E,0x0A,0x30,0x0A,0x30};
-byte msg_boot8 [] = {0x00,0x4E,0x0A,0x00,0x0A,0x00};
-byte msg_boot9 [] = {0x00,0x4E,0x0A,0x01,0x0A,0x01};
-byte msg_bot10 [] = {0x00,0x4E,0x0A,0x02,0x0A,0x02};
-byte msg_bot11 [] = {0x00,0x4E,0x0A,0x03,0x0A,0x03};
-byte msg_bot12 [] = {0x00,0x4E,0x0A,0x04,0x0A,0x04};
-byte msg_bot13 [] = {0x00,0x4E,0x0A,0x05,0x0A,0x05};
-byte msg_bot14 [] = {0x00,0x4E,0x0A,0x06,0x0A,0x06};
-byte msg_bot15 [] = {0x00,0x4E,0x0A,0x07,0x0A,0x07};
-byte msg_bot16 [] = {0x00,0x4E,0x0A,0x08,0x0A,0x08};
-byte msg_bot17 [] = {0x00,0x4E,0x0A,0x09,0x0A,0x09};
-byte msg_bot18 [] = {0x00,0x4E,0x0A,0x0A,0x0A,0x0A};
-byte msg_bot19 [] = {0x00,0x4E,0x0A,0x0B,0x0A,0x0B};
-byte msg_bot20 [] = {0x00,0x4E,0x0A,0x0C,0x0A,0x0C};
-byte msg_bot21 [] = {0x00,0x4E,0x0A,0x0D,0x0A,0x0D};
-byte msg_bot22 [] = {0x00,0x4E,0x0A,0x0E,0x0A,0x0E};
-byte msg_bot23 [] = {0x00,0x4E,0x0A,0x0F,0x0A,0x0F};
-byte msg_bot24 [] = {0x00,0x4E,0x0A,0x10,0x0A,0x10};
-byte msg_bot25 [] = {0x00,0x4E,0x0A,0x11,0x0A,0x11};
-byte msg_bot26 [] = {0x00,0x4E,0x0A,0x12,0x0A,0x12};
-byte msg_bot27 [] = {0x00,0x4E,0x0A,0x13,0x0A,0x13};
-byte msg_bot28 [] = {0x00,0x4E,0x0A,0x14,0x0A,0x14};
-byte msg_bot29 [] = {0x00,0x4E,0x0A,0x15,0x0A,0x15};
-byte msg_bot30 [] = {0x00,0x4E,0x0A,0x16,0x0A,0x16};
-byte msg_bot31 [] = {0x00,0x4E,0x0A,0x17,0x0A,0x17};
-byte msg_bot32 [] = {0x00,0x4E,0x0A,0x18,0x0A,0x18};
-byte msg_bot33 [] = {0x00,0x4E,0x0A,0x19,0x0A,0x19};
-byte msg_bot34 [] = {0x00,0x4E,0x0A,0x1A,0x0A,0x1A};
-byte msg_bot35 [] = {0x00,0x4E,0x0A,0x1B,0x0A,0x1B};
-byte msg_bot36 [] = {0x00,0x4E,0x0A,0x1C,0x0A,0x1C};
-byte msg_bot37 [] = {0x00,0x4E,0x0A,0x1D,0x0A,0x1D};
-byte msg_bot38 [] = {0x00,0x4E,0x11,0x0F,0x11,0x0F};
-byte msg_bot39 [] = {0x00,0x4E,0x0A,0x1E,0x0A,0x2D};
-byte msg_bot40 [] = {0x00,0x4E,0x10,0xF9,0x10,0xF9};
-byte msg_bot41 [] = {0x00,0x4E,0x0A,0x2F,0x0A,0x2F};
-byte msg_bot42 [] = {0x00,0x4E,0x0A,0x30,0x0A,0x30};
-byte msg_bot43 [] = {0x00,0x44};
-byte msg_bot44 [] = {0x00,0x57};
-byte msg_bot45 [] = {0x00,0x45};
-byte msg_bot46 [] = {0x00,0x30,0x11,0x32,0x1B,0x05,0x18}; // ANALISAR UMIDADE
-byte msg_bot47 [] = {0x00,0x50,0x41,0x6E,0x61,0x6C,0x69,0x73,0x61,0x72,0x20,0x55,0x6D,0x69,0x64,0x61,0x64,0x65};
-byte msg_bot48 [] = {0x00,0x50,0x41,0x6E,0x61,0x6C,0x69,0x73,0x61,0x72,0x20,0x55,0x6D,0x69,0x64,0x61,0x64,0x65};
-byte msg_bot49 [] = {0x00,0x42};
+ 
+byte msg_boot5 [] = {0x0D,0x50,0x43,0x6F,0x6E,0x65,0x78,0x61,0x6F,0x20,0x63,0x6F,0x72,0x72,0x65,0x74,0x61,0x20};
+byte msg_boot6 [] = {0x0d,0x04};
+byte msg_boot7 [] = {0x0D,0x4E,0x0A,0x30,0x0A,0x30};
+byte msg_boot8 [] = {0x0D,0x4E,0x0A,0x00,0x0A,0x00};
+byte msg_boot9 [] = {0x0D,0x4E,0x0A,0x01,0x0A,0x01};
+byte msg_bot10 [] = {0x0D,0x4E,0x0A,0x02,0x0A,0x02};
+byte msg_bot11 [] = {0x0D,0x4E,0x0A,0x03,0x0A,0x03};
+byte msg_bot12 [] = {0x0D,0x4E,0x0A,0x04,0x0A,0x04};
+byte msg_bot13 [] = {0x0D,0x4E,0x0A,0x05,0x0A,0x05};
+byte msg_bot14 [] = {0x0D,0x4E,0x0A,0x06,0x0A,0x06};
+byte msg_bot15 [] = {0x0D,0x4E,0x0A,0x07,0x0A,0x07};
+byte msg_bot16 [] = {0x0D,0x4E,0x0A,0x08,0x0A,0x08};
+byte msg_bot17 [] = {0x0D,0x4E,0x0A,0x09,0x0A,0x09};
+byte msg_bot18 [] = {0x0D,0x4E,0x0A,0x0A,0x0A,0x0A};
+byte msg_bot19 [] = {0x0D,0x4E,0x0A,0x0B,0x0A,0x0B};
+byte msg_bot20 [] = {0x0D,0x4E,0x0A,0x0C,0x0A,0x0C};
+byte msg_bot21 [] = {0x0D,0x4E,0x0A,0x0D,0x0A,0x0D};
+byte msg_bot22 [] = {0x0D,0x4E,0x0A,0x0E,0x0A,0x0E};
+byte msg_bot23 [] = {0x0D,0x4E,0x0A,0x0F,0x0A,0x0F};
+byte msg_bot24 [] = {0x0D,0x4E,0x0A,0x10,0x0A,0x10};
+byte msg_bot25 [] = {0x0D,0x4E,0x0A,0x11,0x0A,0x11};
+byte msg_bot26 [] = {0x0D,0x4E,0x0A,0x12,0x0A,0x12};
+byte msg_bot27 [] = {0x0D,0x4E,0x0A,0x13,0x0A,0x13};
+byte msg_bot28 [] = {0x0D,0x4E,0x0A,0x14,0x0A,0x14};
+byte msg_bot29 [] = {0x0D,0x4E,0x0A,0x15,0x0A,0x15};
+byte msg_bot30 [] = {0x0D,0x4E,0x0A,0x16,0x0A,0x16};
+byte msg_bot31 [] = {0x0D,0x4E,0x0A,0x17,0x0A,0x17};
+byte msg_bot32 [] = {0x0D,0x4E,0x0A,0x18,0x0A,0x18};
+byte msg_bot33 [] = {0x0D,0x4E,0x0A,0x19,0x0A,0x19};
+byte msg_bot34 [] = {0x0D,0x4E,0x0A,0x1A,0x0A,0x1A};
+byte msg_bot35 [] = {0x0D,0x4E,0x0A,0x1B,0x0A,0x1B};
+byte msg_bot36 [] = {0x0D,0x4E,0x0A,0x1C,0x0A,0x1C};
+byte msg_bot37 [] = {0x0D,0x4E,0x0A,0x1D,0x0A,0x1D};
+byte msg_bot38 [] = {0x0D,0x4E,0x11,0x0F,0x11,0x0F};
+byte msg_bot39 [] = {0x0D,0x4E,0x0A,0x1E,0x0A,0x2D};
+byte msg_bot40 [] = {0x0D,0x4E,0x10,0xF9,0x10,0xF9};
+byte msg_bot41 [] = {0x0D,0x4E,0x0A,0x2F,0x0A,0x2F};
+byte msg_bot42 [] = {0x0D,0x4E,0x0A,0x30,0x0A,0x30};
+byte msg_bot43 [] = {0x0D,0x44};
+byte msg_bot44 [] = {0x0D,0x57};
+byte msg_bot45 [] = {0x0D,0x45};
+byte msg_bot46 [] = {0x0D,0x30,0x11,0x32,0x1B,0x05,0x18}; // ANALISAR UMIDADE
+byte msg_bot47 [] = {0x0D,0x50,0x41,0x6E,0x61,0x6C,0x69,0x73,0x61,0x72,0x20,0x55,0x6D,0x69,0x64,0x61,0x64,0x65};
+byte msg_bot48 [] = {0x0D,0x50,0x41,0x6E,0x61,0x6C,0x69,0x73,0x61,0x72,0x20,0x55,0x6D,0x69,0x64,0x61,0x64,0x65};
+byte msg_bot49 [] = {0x0D,0x42};
 
 
 byte msg_rec01[] = {0x23};
 byte msg_rec02[] = {0x00};
 byte msg_rec03[] = {0xFF};
-byte msg_rec04[] = {0x00};
 byte msg_rec05[] = {0xFF};
 byte msg_rec06[] = {0x35,0x2E,0x35,0x4A,0x32,0x30,0x31,0x39};
 byte msg_rec07[] = {0xFF};
-byte msg_rec08[] = {0x00};
 byte msg_rec09[] = {0x10};
 byte msg_reC10[] = {0x42};
 byte msg_reC11[] = {0x20};
 byte msg_reC12[] = {0x21};
-byte msg_reC13[] = {0xFF};
-byte msg_reC14[] = {0x07};
+byte msg_reC13[] = {0x07};
+byte msg_reC14[] = {0x23};
 byte msg_reC15[] = {0x23};
 byte msg_reC16[] = {0xC8};
 byte msg_reC17[] = {0x8F};
-byte msg_reC18[] = {0x00};
 byte msg_reC19[] = {0x5A};
 byte msg_reC20[] = {0xFF};
 byte msg_reC21[] = {0x10};
 byte msg_reC22[] = {0x01};
 byte msg_reC23[] = {0x63};
-byte msg_reC24[] = {0x00};
 byte msg_reC25[] = {0xFE};
 byte msg_reC26[] = {0xFB};
 byte msg_reC27[] = {0xFA};
@@ -118,17 +111,10 @@ byte msg_reC42[] = {0x47};
 byte msg_reC43[] = {0xFC,0xFB,0xFB,0xFB,0xFC,0xFD,0xFF,0x00,0xF8,0xF8,0xF9,0xFA,0xFB,0xFD,0xFE,0xFF};
 byte msg_reC44[] = {0x9E};
 byte msg_reC45[] = {0x41};
-byte msg_reC46[] = {0x00};
 byte msg_reC47[] = {0x01,0xF5};
 byte msg_reC48[] = {0x01,0xEF};
 byte msg_reC49[] = {0x01,0xF7};
-byte msg_reC50[] = {0x00};
 byte msg_reC51[] = {0xFF};
-byte msg_reC52[] = {0xFF};
-byte msg_reC53[] = {0x00};
-byte msg_reC54[] = {0x00};
-byte msg_reC55[] = {0xFF};
-byte msg_reC56[] = {0x00};
 byte msg_reC57[] = {0x01,0xF6};
 byte msg_reC58[] = {0x01,0xF0};
 byte msg_reC59[] = {0xFF};
@@ -151,7 +137,7 @@ byte msg_reC75[] = {0x00};
 byte msg_reC76[] = {0x01,0xF5};
 byte msg_reC77[] = {0x01,0xF0};
 byte msg_reC78[] = {0x01,0xF7};
-byte msg_reC79[] = {0x00};
+byte msg_reC79[] = {0xA2};
 // fim msgs boots
 
 int       byteRead = 0;// variavel que inicia processo de comunicação
@@ -169,6 +155,7 @@ unsigned long timeold           = 0,        // Variavel conta tempo em milis con
 // --- Protótipo das Funções        ---
 void limparVetor        ();
 void rotina_de_partida  ();
+void rotina_conexao     ();
 
 // =================================================================================
 // --- Configurações setup       ---
@@ -183,112 +170,198 @@ void setup (){
 void loop() {
   // rotina de partida do sistema de comunicação
   // tirei do setup e deixei aqui no loop 
- if (cont_partida == 0){
-     rotina_de_partida ();
-     cont_partida = 1;
- }
  
-  while (Serial.available() > 0) {
-        byte  request = Serial.read();
-              msg[c_ctr] = request;
-              my_char = String((char)request);
-              timeold_end = millis();
-              c_ctr++;
-              // proteger estourar o vetor
-              if (c_ctr >=  length){// boa prática de programa
-                  c_ctr = length - 1;
-              }
-              
-     }  //END while (Serial.available())
-  //OK proxima fase definir o melhor método de gatilho 
-  //OK validar o metodo de percorrer o vetor de forma rápida
-  // TENTANDO COM IF E FOR  criar metodo de comparar vetor entrada com resposta pre-definida
-    // CONSIDERO CRIAR UM CASE NO FUTURO
-  // medir tempo  de resposta igual ao computador utilizando write
-  // ao usar print demora 4 vezes mais.
-  // criar condição que ao responder o vetor uma vez se eu enviar novamente
-  // por exemplo o hexa 23 não poderá responder.
-  // ou se no meio do vetor tiver 23 nao responder
-/*
-inicia o role
-	{0x00}; 
-	10ms
-	{0x41,0x21};
-RECEBER
-	{0x23};
-responde
-	{0x40};
-RECEBER
- 	{0x00};
-responde
-	{0x00,0x42};
-	10ms
-	{0x00,0x50,0x43,0x6F,0x6E,0x65,0x78,0x61,0x6F,0x20,0x63,0x6F,0x72,0x72,0x65,0x74,0x61,0x20};
-	10ms
-	{0x00,0x04};
-parei aqui
-
-se receber
-{0x35,0x2E,0x35,0x4A,0x32,0x30,0x31,0x39};
-responde
-{0x00,0x4E,0x0A,0x30,0x0A,0x30};
-se receber
- {0x00};
-responde
-{0x00,0x4E,0x0A,0x00,0x0A,0x00};
-
-
-
-
-
-*/
-    if (millis() - timeold_end >= 1000){   // LIMPA TUDO A CADA 1 S SEM COMUNICAÇÃO
-        for (size_t i = 0; i < c_ctr; i++) { // debug vetor recebido
-          //Serial.write(msg[i]);
-        } // fim debug recebido
-        for (size_t i = 0; i < c_ctr; i++) {
-          // criei uma lista de vetores com cada resposta em sequencia.
-        // tem que colocar condição de fazer so uma vez
-          if (msg[i] == msg_rec01[0]){ // se receber 0x23 responder 0x40
-            Serial.write(msg_boot3, sizeof(msg_boot3)); //{0x40};
-          }
-        // tem que colocar condição de fazer so uma vez
-          if (msg[i] == msg_rec02[0]){ // msg_rec02[] = {0x00};se receber 0x00 responder {0x00,0x42};
-            Serial.write(msg_boot4, sizeof(msg_boot4)); // {0x00,0x42};
-            delay(10); 
-            Serial.write(msg_boot5, sizeof(msg_boot5));
-            Serial.flush();
-            delay(10); //msg_boot6
-            Serial.write(msg_boot6, sizeof(msg_boot6));
-          }
-        } // END FOR
+ 
+while (Serial.available() > 0) {
+      byte request = Serial.read();
+      msg[c_ctr]   = request;
+      my_char      = String((char)request);
+      timeold_end  = millis();
+      c_ctr++;
+      if (c_ctr >=  length) c_ctr = length - 1; // proteger estourar o vetor
+}  //END while (Serial.available())
+// testa as mensagens recebidas fora do while
+// essa solução ganha velocidade
+rotina_conexao     ();
+  // CONSIDERO CRIAR UM CASE NO FUTURO
+if (millis() - timeold_end >= 5000){   // LIMPA TUDO A CADA 1 S SEM COMUNICAÇÃO
+    for (size_t i = 0; i < c_ctr; i++) //Serial.write(msg[i]); // DEBUG
         // limpa vetor
           timeold_end = millis();
           my_char.remove(0, my_char.length());
           limparVetor();
           c_ctr  = 0;
-       }
-  /*
-vetor recebido
-1    2    3    4    5    6    7
+}
 
-DISPARADO
-1    2    3    4    5    6    7
-0x01 0xF5 0x02 0x0C 0x02 0x15 0x0D
-                  0x23
-  */
 } // END void loop()
 // =================================================================================
+void rotina_conexao() {
+    for (size_t i = 0; i < c_ctr; i++) {
+        // criei uma lista de vetores com cada resposta em sequencia.
+        if (msg[i] == 0xA2 && cont_partida == 0) { // se receber 0x23 responder 0x40
+            Serial.write(0x0D);//{0x00};
+            delay(10);
+            Serial.write(0x41);//{0x41,0x21};
+            Serial.write(0x21);
+            msg[i] = 0xA3;
+            cont_partida = 1;
+        }
+        if (msg[i] == 0x23 && cont_partida == 1) { // se receber 0x23 responder 0x40
+           delay(10);
+           Serial.write(0x40);
+           msg[i] == 0xA3; // ESTOU COLOCANDO ALGUMA COISA NO LUGAR PARA O IF NAO RODAR DE NOVO
+           cont_partida = 2;
+           delay(10);
+           Serial.write(0x0D);//{0x41,0x21};
+           Serial.write(0x42);
+           delay(10);
+           Serial.write(msg_boot5, sizeof(msg_boot5));
+           Serial.flush(); 
+           delay(140);
+           Serial.write(0x0D);//{0x41,0x21};
+           Serial.write(0x4D);
+           msg[i] == 0xA3; // ESTOU COLOCANDO ALGUMA COISA NO LUGAR PARA O IF NAO RODAR DE NOVO
+          cont_partida = 3;
+         }
+         
+         if (msg[i] == 0x35 && msg[i+1] == 0x2E && cont_partida == 3){
+          cont_partida = 4;
+          delay(9);
+          Serial.write(msg_boot7, sizeof(msg_boot7));
+          Serial.flush();
+          delay(9); 
+          Serial.write(msg_boot8, sizeof(msg_boot8));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_boot9, sizeof(msg_boot9));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot10, sizeof(msg_bot10));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot11, sizeof(msg_bot11));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot12, sizeof(msg_bot12));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot13, sizeof(msg_bot13));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot14, sizeof(msg_bot14));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot15, sizeof(msg_bot15));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot16, sizeof(msg_bot16));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot17, sizeof(msg_bot17));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot18, sizeof(msg_bot18));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot19, sizeof(msg_bot19));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot20, sizeof(msg_bot20));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot21, sizeof(msg_bot21));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot22, sizeof(msg_bot22));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot23, sizeof(msg_bot23));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot24, sizeof(msg_bot24));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot25, sizeof(msg_bot25));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot26, sizeof(msg_bot26));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot27, sizeof(msg_bot27));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot28, sizeof(msg_bot28));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot29, sizeof(msg_bot29));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot30, sizeof(msg_bot30));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot31, sizeof(msg_bot31));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot32, sizeof(msg_bot32));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot33, sizeof(msg_bot33));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot34, sizeof(msg_bot34));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot35, sizeof(msg_bot35));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot36, sizeof(msg_bot36));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot37, sizeof(msg_bot37));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot38, sizeof(msg_bot38));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot39, sizeof(msg_bot39));
+          Serial.flush(); 
+         }
+         if (msg[i] == 0xFC && msg[i+1] == 0xFB && cont_partida == 4){
+          cont_partida = 5;
+          delay(24);
+          Serial.write(msg_bot40, sizeof(msg_bot40));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot41, sizeof(msg_bot41));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot42, sizeof(msg_bot42));
+          Serial.flush(); 
+          delay(10);
+          Serial.write(msg_bot43, sizeof(msg_bot43));
+          Serial.flush(); 
+         }
+
+         
+         
+         
+         
+         
+    } // END FOR
+} // END void rotina_conexao ()
+// =================================================================================
+
 void rotina_de_partida (){ // rotina validade
-  Serial.write(msg_boot1, sizeof(msg_boot1));//{0x00};
+  Serial.write(0x00);//{0x00};
   delay(10);
-  Serial.write(msg_boot2, sizeof(msg_boot2));//{0x41,0x21};
+  Serial.write(0x41);//{0x41,0x21};
+  Serial.write(0x21);
 } //void rotina_de_partida (){
               
 void limparVetor() {
       for (size_t i = 0; i < length; i++) msg[i] = ' ';
       msg[0] = '\0';
-      str.remove(0, str.length());
+      my_char.remove(0, my_char.length());
       c_ctr         = 0; //= '\0'
 } // END void limparVetor()
 // =================================================================================
